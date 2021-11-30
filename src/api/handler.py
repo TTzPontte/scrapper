352f4cef338ac423b815f6612bce42616a8d4f8f
Guezin <leandroguezinjunior@gmail.com>
2021-11-30 16:36:55 -0300

    refactor: add pip install buildspec

--- src/api/handler.py
+++ src/api/handler.py
@@ -1,9 +1,7 @@
 import json
-import requests
+from selenium.webdriver import Firefox
 
 def handler(event, context):
-    response = requests.get('https://api.github.com/orgs/Pontte/repos')
-    print(response.text)
     return {
         "statusCode": 200,
         "body": json.dumps({
