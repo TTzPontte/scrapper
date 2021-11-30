ea7fbac1592b9947dc72145fbca93c422d56378c
Guezin <leandroguezinjunior@gmail.com>
2021-11-30 15:43:08 -0300

    refactor: update structure

--- src/api/handler.py
+++ src/api/handler.py
@@ -1,6 +1,9 @@
 import json
+import requests
 
 def handler(event, context):
+    response = requests.get('https://api.github.com/orgs/Pontte/repos')
+    print(response.text)
     return {
         "statusCode": 200,
         "body": json.dumps({
